182a01f16dfb29e2b3e47c6a218c1ea3258e832f
victor <victor.murithi@student.moringaschool.com>
2023-09-15 12:48:36 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, event){
+const thankYou =[];
+
+    for(let n= 0;n < names.length;n++){
+        const note = [`Thank you, ${names[n]}, for the wonderful ${event} gift!`];
+        thankYou.push(...note);
+        console.log(thankYou);
+    }
+    return thankYou;
+}
+function countDown(firstNumber){
+    for(let p = firstNumber;p>=0;p--)
+    console.log(p);
+}
